b4ef5c78768e0fb4a12ba5b8e465c7e12ed02866
Abdullah Shakur <109961338+37148149@users.noreply.github.com>
2023-05-21 23:52:51 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+// changing navbar
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle
+        ('window-scroll', window.scrollY > 0)
+})
+
+// open/hide faq
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open')
+
+        const icon = faq.querySelector('.faq_icon i');
+        if (icon.className === "fa-solid fa-plus") {
+            icon.className = "fa-solid fa-minus"
+        } else {
+            icon.className = "fa-solid fa-plus"
+        }
+    })
+
+})
